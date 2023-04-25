d67c279826cf32b776ce0da4360241e2e4d4427d
Crowdbotics <team@crowdbotics.com>
2023-04-25 04:31:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/babel_40194/MainActivity.java
@@ -0,0 +1,15 @@
+package com.babel_40194;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "babel_40194";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "babel-40194",
+  url: "https://babel-40194.botics.co",
+  api: "https://babel-40194.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
